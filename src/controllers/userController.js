ab4001ea546fd3b72a62c0647f6528ad6aa85ef1
LeonardoNXT <leonardoffice12@gmail.com>
2025-06-23 19:28:29 -0300

    ADD A NEW ORIGIN 2

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -115,7 +115,7 @@ exports.login = async (req, res) => {
       res.setHeader("Access-Control-Allow-Credentials", "true");
       res.setHeader(
         "Access-Control-Allow-Origin",
-        "https://feelsystem.vercel.app"
+        "https://feelcore.onrender.com"
       );
       res.setHeader("Vary", "Origin");
     }
